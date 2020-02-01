756d2c4b1cf324942da7f5789cb1c9659d7a1a8c
Corey Clippinger <coreyclip@gmail.com>
2020-02-01 11:48:58 -0800

    began fleshing out contact form

--- app/forms.py
+++ app/forms.py
@@ -3,3 +3,10 @@ from wtforms import StringField
 
 class MessageForm(FlaskForm):
     message = StringField('Message')
+
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name')
+    last_name = StringField('Last Name')
+    cell_phone = StringField('Cell Phone')
+    email = StringField('Email')
